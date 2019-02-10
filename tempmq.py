23237685807217b6e8b48a4d1d8746b6451588f2
carloarp <47327416+carloarp@users.noreply.github.com>
2019-02-10 17:43:31 +0000

    Add files via upload

---
+++ tempmq.py
@@ -0,0 +1,72 @@
+import paho.mqtt.client as mqtt
+import subprocess
+import datetime
+import time
+import smbus		#import SMBus module of I2C
+from time import sleep  #import sleep
+import math
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+
+# TMP007 address, 0x40(64)
+# Select configuration register, 0x02(02)
+#		0x1540(5440)	Continuous Conversion mode, Comparator mode
+data = [0x1540]
+bus.write_i2c_block_data(0x40, 0x02, data)
+time.sleep(1)
+
+def read_raw_data(addr):
+		#Read raw 16-bit value
+		high = bus.read_byte_data(Device_Address, addr)
+		low = bus.read_byte_data(Device_Address, addr+1)
+
+		#concatenate higher and lower value
+		value = ((high << 8) | low)
+
+		#to get signed value from module
+		if(value > 32768):
+			value = value - 65536
+		return value
+
+def on_message (client,userdata,message):
+		print("Received message:{} on topic".format(message.payload, message.topic))
+	
+
+client = mqtt.Client()
+client.tls_set(ca_certs="mosquitto.org.crt", certfile="client.crt",keyfile="client.key")
+client.connect("test.mosquitto.org",port=8884)
+
+while True:
+		
+		data = bus.read_i2c_block_data(0x40, 0x01, 2)
+
+	
+		# Convert the data to 14-bits
+		cTemp = ((data[0] * 256 + (data[1] & 0xFC)) / 4)
+		if cTemp > 8191 :
+			cTemp -= 16384
+		cTemp = cTemp * 0.03125
+		fTemp = cTemp * 1.8 + 32
+		# Output data to screen
+		print ("Object Temperature in Celsius : %.2f C" %cTemp)
+		time.sleep(1)
+
+		#sleep(1)
+
+		client.publish("IC.embedded/apollostark/test", cTemp)
+
+		#client.on_message = on_message
+		#client.subscribe("IC.embedded/apollostark/#")
+		client.loop()
+		client.loop_start()
+		client.loop_stop()
+
+		#while True:
+			#time.sleep(3)
+			#client.loop_forever()
+
+		#client.disconnect()
+		print("Disconnected!")
+		
+# ---------------------------------------------------------	
