f5f5e706ccf535a5e17ee50284e2239308155e2a
carloarp <47327416+carloarp@users.noreply.github.com>
2019-02-11 14:44:00 +0000

    Add files via upload

--- mygraph2/app/src/main/java/com/example/mygraph2/MainActivity.java
+++ mygraph2/app/src/main/java/com/example/mygraph2/MainActivity.java
@@ -10,8 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.Viewport;
@@ -19,12 +22,15 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.PointsGraphSeries;
 
 import java.util.Random;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity
 {
     private static final Random RANDOM = new Random();
     private PointsGraphSeries<DataPoint> series;
-    private int lastX = 0;
+    SimpleDateFormat sdf = new SimpleDateFormat("mm:ss:");
+    private double lastX = 0;
+    private double lastY = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,9 +43,22 @@ public class MainActivity extends AppCompatActivity
 
         //data
         series = new PointsGraphSeries<DataPoint>();
+        series = new PointsGraphSeries<>(getDataPoint());
         graph.addSeries(series);
 
-        // customize a little bit viewport
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+            @Override
+            public String formatLabel(double value, boolean isValueX){
+                if (isValueX)
+                {
+                    return sdf.format(new Date((long)value));
+                }else
+                {
+                    return super.formatLabel(value, isValueX);
+                }
+            }
+        });
+
         Viewport viewport = graph.getViewport();
         viewport.setYAxisBoundsManual(true);
         viewport.setMinY(0);
@@ -53,7 +72,20 @@ public class MainActivity extends AppCompatActivity
         viewport.setScalable(true); // enables horizontal zooming and scrolling
         viewport.setScalableY(true); // enables vertical zooming and scrolling
 
+    }
 
+    private DataPoint[] getDataPoint()
+    {
+        DataPoint[] dp = new DataPoint[]
+        {
+                new DataPoint(new Date().getTime(),lastY),
+                new DataPoint(new Date().getTime(),lastY),
+                new DataPoint(new Date().getTime(),lastY),
+                new DataPoint(new Date().getTime(),lastY),
+                new DataPoint(new Date().getTime(),lastY),
+
+        };
+        return dp;
     }
 
     @Override
@@ -71,6 +103,10 @@ public class MainActivity extends AppCompatActivity
 
                         @Override
                         public void run() {
+
+                            //lastX = 0;
+                            lastY = lastY+1;
+
                             addEntry();
                             //series.resetData(DataPoint);
                                                     }
@@ -90,7 +126,7 @@ public class MainActivity extends AppCompatActivity
     // add random data to graph
     private void addEntry() {
         // here, we choose to display max 10 points on the viewport and we scroll to end
-        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 3600);
+        //series.appendData(new DataPoint(lastX++, lastY++), true, 3600);
     }
 
 }
