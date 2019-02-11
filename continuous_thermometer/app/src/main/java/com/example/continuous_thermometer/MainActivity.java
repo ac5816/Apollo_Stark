c1efb4ae90cf3da8441f9598f75a28a57c48e1d0
ac5816 <40827965+ac5816@users.noreply.github.com>
2019-02-11 16:32:09 +0000

    App that displays current time, temperature and graph

--- continuous_thermometer/app/src/main/java/com/example/continuous_thermometer/MainActivity.java
+++ continuous_thermometer/app/src/main/java/com/example/continuous_thermometer/MainActivity.java
@@ -1,118 +1,182 @@
 package com.example.continuous_thermometer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
-import java.io.UnsupportedEncodingException;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+    MqttHelper mqttHelper;
+
+    TextView TempReceived;
+    TextView TimeReceived;
+
+    final String DEGREE = "\u2103";
+
+    //Graph variables
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> seriesLine;
+    private PointsGraphSeries<DataPoint> series;
+    SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss:");
+    //private double lastX = 0;
+    double lastY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String clientId = MqttClient.generateClientId();
+        startMqtt();
 
-        //The URL of the Mosquitto Broker is 192.168.9.100:1883
-        final  MqttAndroidClient client = new MqttAndroidClient(this.getApplicationContext(), "tcp://test.mosquitto.org:1883", clientId);
+        // we get a graph view instance
+        GraphView graph = (GraphView) findViewById(R.id.graph);
 
-        client.setCallback(new MqttCallbackHandler(client));//This is here for when a message is received
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMaxY(50);
+        viewport.setScrollable(true);
+        viewport.setScrollable(true); // enables horizontal scrolling
+        viewport.setScalable(true); // enables horizontal zooming and scrolling
+        //dat
 
-        MqttConnectOptions options = new MqttConnectOptions();
+        seriesLine = new LineGraphSeries<DataPoint>();
+        seriesLine = new LineGraphSeries<>(getDataPoint());
 
-        try {
-            IMqttToken token = client.connect(options);
+        series = new PointsGraphSeries<DataPoint>();
+        series = new PointsGraphSeries<>(getDataPoint());
 
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                    Log.d("mqtt", "onSuccess");
-//-----------------------------------------------------------------------------------------------
-                    //PUBLISH THE MESSAGE
-                    MqttMessage message = new MqttMessage("Hello, I am an Android Mqtt Client.".getBytes());
-                    message.setQos(2);
-                    message.setRetained(false);
+        graph.addSeries(series);
+        graph.addSeries(seriesLine);
 
-                    String topic = "IC.embedded/apollostark/test";
-
-                    try {
-                        client.publish(topic, message);
-                        Log.i("mqtt", "Message published");
-
-                        // client.disconnect();
-                        //Log.i("mqtt", "client disconnected");
-
-                    } catch (MqttPersistenceException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-
-                    } catch (MqttException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-//-----------------------------------------------------------------------------------------------
-
-                    String subtopic = "IC.embedded/apollostark/#";
-                    int qos = 1;
-                    try {
-                        IMqttToken subToken = client.subscribe(subtopic, qos);
-                        subToken.setActionCallback(new IMqttActionListener() {
-                            @Override
-                            public void onSuccess(IMqttToken asyncActionToken) {
-                                // The message was published
-                                Log.i("mqtt", "subscription success");
-                            }
-
-                            @Override
-                            public void onFailure(IMqttToken asyncActionToken,
-                                                  Throwable exception) {
-                                // The subscription could not be performed, maybe the user was not
-                                // authorized to subscribe on the specified topic e.g. using wildcards
-                                Log.i("mqtt", "subscription failed");
-
-                            }
-                        });
-
-
-
-                    } catch (MqttException e) {
-                        e.printStackTrace();
-                    }
-
-//---------------------------------------------------------------------------
-
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+            @Override
+            public String formatLabel(double value, boolean isValueX){
+                if (isValueX)
+                {
+                    return sdf.format(new Date((long)value));
+                }else
+                {
+                    return super.formatLabel(value, isValueX);
                 }
+            }
+        });
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Log.d("mqtt", "onFailure");
+        //graph.getGridLabelRenderer().setHumanRounding(false);
+        graph.getGridLabelRenderer().setNumHorizontalLabels(10);
+        graph.getGridLabelRenderer().setNumVerticalLabels(40);
+        graph.getGridLabelRenderer().setTextSize(10f);
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Time");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Temperature");
 
-                }
+        series.setSize(10);
 
-            });
+        TempReceived = findViewById(R.id.Temp);
+        TimeReceived = findViewById(R.id.Time);
 
-
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-
+        setTitle("Continuous Thermometer");
     }
 
+    private DataPoint[] getDataPoint()
+    {
+        DataPoint[] dp = new DataPoint[]
+                {
+                        new DataPoint(new Date().getTime(),lastY),
+                        //new DataPoint(new Date().getTime(),lastY),
+                        //new DataPoint(new Date().getTime(),lastY),
+                        //new DataPoint(new Date().getTime(),lastY),
+                        //new DataPoint(new Date().getTime(),lastY),
 
-}//End of Activity class
+                };
+        return dp;
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // we're going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
 
+            @Override
+            public void run() {
+                // we add 100 new entries
+                //for (int i = 0; i < 200; i++) {
+                while (true){
+                    runOnUiThread(new Runnable() {
 
+                        @Override
+                        public void run() {
+
+                            //lastX = 0;
+                            lastY = lastY+1;
+
+                            addEntry();
+                            //series.resetData(DataPoint);
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(2200);     //sampling every 1.1s
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // add random data to graph
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(new Date().getTime(),lastY), true, 3600);
+        seriesLine.appendData(new DataPoint(new Date().getTime(),lastY), true, 3600);
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("mqtt_message_received",mqttMessage.toString());
+                String temperature = mqttMessage.toString();
+                lastY = Double.parseDouble(temperature.substring(24, 29));
+                TempReceived.setText(temperature.substring(24,29)+ DEGREE);
+                TimeReceived.setText(temperature.substring(7,15));
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+}
